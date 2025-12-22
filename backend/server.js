e3ac4d0f0fe78f074b0f46f3337a897912cf9355
codebrian <codebrian@kali>
2025-12-22 18:26:07 +0200

    production-setup

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use("/api/users", userRoutes);
 if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-    app.get("*", (req,res) => {
+    app.get("/*splat", (req,res) => {
         res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
     })
 }
